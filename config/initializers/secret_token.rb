6a40a9f9b1551f53fc6677320947e74893e3f86a
Apprentice <apprentice@dbc-19.local>
2013-10-30 10:39:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryOverflow::Application.config.secret_token = '865173ee8b52f6c2ca11d3f439735357dfbcd6066eb90638518de5233b3ae6e6a49f40f12c84d9269c14c9b683a996c94e33c068fb9196411a68aa4066a2dd60'
