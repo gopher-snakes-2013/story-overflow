1ecf5c519f9374ccd1a686958098f0da53d232da
Dev Bootcamp <Apprentice@dbc33.local>
2013-11-01 00:56:58 -0700

    added comments to the ajax

--- app/assets/javascripts/ajaxy.js
+++ app/assets/javascripts/ajaxy.js
@@ -2,17 +2,23 @@ $(document).ready(function() {
   $('#new_story').click(function(event){
     event.preventDefault();
     $('.hidden').removeClass('hidden')
-  });
+  }); // upon clicking 'Create a Story', removes the 'hidden' class from the form
+
   $('.submit-button input').on('click', function(e){
     e.preventDefault();
     var $form = $('form');
     console.log($form)
     $.ajax({
-      url: $form.prop('action'),
-      type: $form.prop('method'),
-      data: $form.serialize()
+      url: $form.prop('action'),  //  #=> /stories | ex: <form action="/stories">
+      type: $form.prop('method'), //  #=> "post"   | ex: <form method="post">
+      data: $form.serialize()     // {:story => {:title => ____, :description => ____}} *gets passed to params
     }).done(function(new_story){
-      $('.story-list').append(new_story)
+      $('.story-list').append(new_story) // receives a string [Look @ stories_controller.rb, create method WHOA!]
     })
   })
+
+  // $('#story-container').on('ajax:success', '.delete_button', function(x) {
+  //   console.log(x)
+
+  //  Note in reference to commented out code directly above: Via event delegation, we were able to attach a listener onto the 'story-container' div which made it listen to all children that have a class of 'delete_button'. [Look @ _story.html.erb] Since the link_to "DELETE THIS SHIT" has remote: true, the href will run its route to the controller... the on('ajax:success')  means that the interaction with the controller #action was successful (the story got deleted), and then WE have to do JQuery to make the necessary visual changes on the client side which shows the story was actually deleted :)
 });
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -12,6 +12,16 @@ class StoriesController < ApplicationController
     @story = Story.new(params[:story])
     if @story.save
       render :text => render_to_string(:partial => 'story', :locals => {:story => @story})
+
+      # [Look @ ajaxy.js]:
+      # BIG PICTURE: The result of the above (an html in string form) gets passed to ajax.done as an 'argument' that we chose to call 'new_story'
+      # Note: render_to_string, takes two arguments
+      #   1) Which partial to send data to [Look @ _story.html.erb]
+      #   2) A hash, that gets sent and can be accessed by the partial :)
+
+      # Render EX: render :text => "zebra"  <-- `new_story = "zebra"`
+      # Render EX: render :json => {:apple => "moose"} <-- `new_story = {:apple => "moose"}
+
     else
       redirect_to root_path
     end
